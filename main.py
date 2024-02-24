d9930467282530847c28de8929ddfc5b6815cb40
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2024-02-24 21:21:55 +0700

    Final: Projek SISTEM PERSAMAAN LINIER N VARIABEL sudah diselesaikan

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from metod.splnv import splnv_metod as sm
+from metod.splnv import SplnvMetod as Sm
+from metod.splnv import InputanSplnv as Is
 
-f = sm([[1,2],[2,3]],[2,3])
 if __name__ == "__main__":
-    print("hasil perkalian:")
-    print(f.boot_system_persamaan())
-    print(f.nama)
+    print("Selamat Datan Di Sistem Persamaan Linier N variabel")
+    print("===================================================")
+    j_persamaan = int(input("Jumlah Persamaan : "))
+    j_variabel = int(input("Jumlah Variabel : "))
+    inp = Is(j_persamaan, j_variabel)
+    inp_h = inp.inputan_sistem_persamaan()
+    hasil = Sm(inp_h[0],inp_h[2])
+    hasil_h = hasil.hasil_system_persamaan()
+    print()
+    print("========================")
+    print("    Hasil Persamaan     ")
+    print("========================")
+    for i in range(len(hasil_h)):
+        print("Nilai {} : {}".format(inp_h[1][i],hasil_h[i]))
--- metod/splnv.py
+++ metod/splnv.py
@@ -2,11 +2,34 @@ import numpy as np
 import pandas as pd
 import  matplotlib.pyplot as plt
 
-class splnv_metod:
-    nama="Sistem Persamaan Linier"
+class SplnvMetod:
+    nama="Sistem Persamaan Linier n variabel"
     def __init__(self, matrix, c):
         self.matrix = matrix
         self.c = c
-    def boot_system_persamaan(self):
+    def hasil_system_persamaan(self):
         return np.dot(np.linalg.inv(self.matrix), self.c)
 
+
+class InputanSplnv:
+    matrix = []
+    variabel = []
+    konstan = []
+    def __init__(self, jumlah_persamaan, jumlah_variabel):
+        self.jumlah_pesamaan = jumlah_persamaan
+        self.jumlah_variabel = jumlah_variabel
+        self.jumlah_konstan = jumlah_persamaan
+    def inputan_sistem_persamaan(self):
+        print("Masukan Nilai kofision :")
+        for i in range(self.jumlah_pesamaan):
+            lis_kofision = []
+            for j in range(self.jumlah_variabel):
+                lis_kofision.append(int(input("Masukan nilai kofision ke-{} : ".format(i))))
+            self.matrix.append(lis_kofision)
+        print("Masukan variabelnya :")
+        for i in range(self.jumlah_variabel):
+            self.variabel.append(str(input("Masukan nilai variabelnya ke-{} :".format(i))))
+        print("Masukan Nilai Konstan :")
+        for i in range(self.jumlah_konstan):
+            self.konstan.append(int(input("Masukan nilai konstan ke-{} :".format(i))))
+        return [self.matrix, self.variabel, self.konstan]
